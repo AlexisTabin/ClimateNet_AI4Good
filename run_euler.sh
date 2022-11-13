92d94aaceabe0f76bbcebe3c7c76682b14510b59
Alexis <alexis.tabin@gmail.com>
2022-11-13 14:41:48 +0100

    Allow more time and try less epochs

--- run_euler.sh
+++ run_euler.sh
@@ -16,4 +16,4 @@ mv lsf* old_lsf/
 # bsub -n 1 -W 12:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" <~/ClimateNet_AI4Good/run.sh
 
 # Trying with twice more memory
-bsub -n 4 -W 12:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" <~/ClimateNet_AI4Good/run.sh
+bsub -n 4 -W 24:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" <~/ClimateNet_AI4Good/run.sh
