f024828cfe809623d4fec36432544dbe411c9aa0
Alexis <alexis.tabin@gmail.com>
2022-11-23 12:28:00 +0100

    Change command to run on euler

--- run_euler.sh
+++ run_euler.sh
@@ -4,7 +4,7 @@ TAG=$(git rev-parse --short HEAD)
 
 mv lsf* old_lsf/
 
-bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_model0==GeForceGTX1080Ti]" "source $(pwd)/run.sh cgnet"
+bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]"  "source $(pwd)/run.sh cgnet"
 # bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" "source $(pwd)/run.sh segnetresnet"
 # bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" "source $(pwd)/run.sh unetresnet"
 # bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" "source $(pwd)/run.sh unet"
@@ -12,10 +12,13 @@ bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[
 # bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" "source $(pwd)/run.sh upernet"
 
 # command to run on Euler cluster : 
-# This command took ages
+# !!!nThese commands took ages !!!
+# bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_model0==GeForceGTX1080Ti]" "source $(pwd)/run.sh cgnet"
 # bsub -n 40 -B -N -R "rusage[mem=4500,ngpus_excl_p=8]" -R "select[gpu_model0==GeForceGTX1080Ti]" "python example.py"
+
 # This one is working straight away 
 # bsub -n 40 -B -N -R "rusage[mem=4500,ngpus_excl_p=8]" "python example.py"
+
 # To put the output direct in outputs folder
 # bsub -n 40 -oo "outputs/output" -B -N -R "rusage[mem=4500,ngpus_excl_p=8]" "python example.py"
 
