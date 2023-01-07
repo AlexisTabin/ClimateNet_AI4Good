32be8616650fbcb711edd299e28e2a5508f84659
AlexisTabin <atabin@student.ethz.ch>
2023-01-07 19:08:24 +0100

    try erfnet

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=SegNet
+#SBATCH --job-name=ErfNet
 #SBATCH --gpus=1
 #SBATCH --gres=gpumem:24G
 #SBATCH --mem-per-cpu=16G
-#SBATCH --output=SegNet.out
-#SBATCH --error=SegNet.err
+#SBATCH --output=erfnet.out
+#SBATCH --error=erfnet.err
 #SBATCH --time=240
 
 python3 ./train.py 
