e86f844db7ced6bc277c63291a282e9cd73dac9f
AlexisTabin <atabin@student.ethz.ch>
2023-01-07 15:51:08 +0100

    Remove unnecessary stuff

--- run.sh
+++ run.sh
@@ -9,38 +9,3 @@
 #SBATCH --time=240
 
 python3 ./train.py 
-
-
-### --- OLD VERSION --- ###
-
-# model=$1
-# location=euler
-
-# if [[ $location == euler ]]
-# then
-# 	user="atabin"
-#     data_folder="/cluster/work/igp_psr/ai4good/group-1b/data/"
-#     scratch_folder="/cluster/scratch/$user"
-#     save_dir="$scratch_folder/$model/"
-#     checkpoint_path="$scratch_folder/checkpoints/$model/"
-# else
-# 	user="alexis"
-#     data_folder="/media/alexis/MyPassport/ai4good/"
-#     save_dir="$data_folder/$model/"
-#     checkpoint_path="$data_folder/checkpoints/$model/"
-# fi
-
-
-# echo "Running on $location with following parameters : "
-# echo "User : $user"
-# echo "Model : $model"
-# echo "Data folder : $data_folder"
-# echo "Saving directory : $save_dir"
-# echo "Checkpoint path : $checkpoint_path"
-
-# python ~/ClimateNet_AI4Good/run.py $model \
-#     --data_dir $data_folder \
-#     --checkpoint_path $checkpoint_path \
-#     --save_dir $save_dir
-
-# echo "Done!"
--- run_euler.sh
+++ run_euler.sh
@@ -12,32 +12,3 @@ TAG=$(git rev-parse --short HEAD)
 source ~/.bashrc
 conda activate climatenet
 sbatch < run.sh
-
-
-
-
-
-# ------------- OLD COMMANDS ----------------
-
-# bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]"  "source $(pwd)/run.sh erfnet"
-# bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" "source $(pwd)/run.sh segnetresnet"
-# bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" "source $(pwd)/run.sh unetresnet"
-# bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" "source $(pwd)/run.sh unet"
-# bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" "source $(pwd)/run.sh cgnet"
-# bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" "source $(pwd)/run.sh upernet"
-
-# command to run on Euler cluster : 
-# !!! These commands took ages !!!
-# bsub -n 4 -W 8:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_model0==GeForceGTX1080Ti]" "source $(pwd)/run.sh cgnet"
-# bsub -n 40 -B -N -R "rusage[mem=4500,ngpus_excl_p=8]" -R "select[gpu_model0==GeForceGTX1080Ti]" "python example.py"
-
-# This one is working straight away 
-# bsub -n 40 -B -N -R "rusage[mem=4500,ngpus_excl_p=8]" "python example.py"
-
-# To put the output direct in outputs folder
-# bsub -n 40 -oo "outputs/output" -B -N -R "rusage[mem=4500,ngpus_excl_p=8]" "python example.py"
-
-# This one lead to out of memory error
-# bsub -n 1 -W 12:00 -J $TAG -B -N -R "rusage[mem=4096,ngpus_excl_p=8]" -R "select[gpu_mtotal0>=8192]" <~/ClimateNet_AI4Good/run.sh
-
-
